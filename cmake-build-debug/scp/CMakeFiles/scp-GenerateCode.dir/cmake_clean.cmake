d4eb94fa6d2a388219e5c549695f1f2ff108fa8e
lexfor <thetim182001@mail.ru>
2020-11-09 16:31:22 +0300

    Fixed for 0.5.0 build

---
+++ cmake-build-debug/scp/CMakeFiles/scp-GenerateCode.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scp-GenerateCode"
+  "scp.gen_cache.missed"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/scp-GenerateCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
