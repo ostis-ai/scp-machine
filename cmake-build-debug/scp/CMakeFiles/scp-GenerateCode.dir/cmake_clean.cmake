b8cea00fb74c7e0016d42a9a9c96e2ad3532c151
Daniil Shunkevich <shu.dv@tut.by>
2021-01-23 20:39:11 +0300

    cmake-build-debug removed

--- cmake-build-debug/scp/CMakeFiles/scp-GenerateCode.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/scp-GenerateCode"
-  "scp.gen_cache.missed"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/scp-GenerateCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
