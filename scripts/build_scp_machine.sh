6b6a20b7776a569ca78fb755c2370c516318730f
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-03-27 16:14:24 +0300

    [scripts] Use common scripts from ostis-scripts

--- scripts/build_scp_machine.sh
+++ scripts/build_scp_machine.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-CURRENT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
-source "${CURRENT_DIR}/formats.sh"
+CURRENT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+OSTIS_SCRIPTS_DIR="${CURRENT_DIR}/ostis-scripts"
+source "${OSTIS_SCRIPTS_DIR}/message-scripts/messages.sh"
 
 if [[ -z "${SCP_MACHINE_CMAKE_PATH}" || -z "${SCP_MACHINE_CXX_SOURCES_PATH}"
   || -z "${SCP_MACHINE_BUILD_PATH}" || -z "${BINARY_PATH}" ]];
@@ -10,10 +11,10 @@ then
   source "${CURRENT_DIR}/set_vars.sh"
 fi
 
-stage "Build scp-machine"
+info "Build scp-machine"
 
-BUILD_SCRIPTS="${SCP_MACHINE_CMAKE_PATH}/../sc-machine/scripts/build-scripts"
+BUILD_SCRIPTS="${OSTIS_SCRIPTS_DIR}/build-scripts"
 "${BUILD_SCRIPTS}/build_cxx_project.sh" -p "${SCP_MACHINE_CMAKE_PATH}" -s "${SCP_MACHINE_CXX_SOURCES_PATH}" \
   -b "${SCP_MACHINE_BUILD_PATH}" --cmake-arg "-DSC_BIN_PATH=${BINARY_PATH}" "$@"
 
-stage "scp-machine is built successfully"
+info "scp-machine is built successfully"
